0f7856dbf4fa9ae154dc623f65da9c11debc6c4e
Kesab Nayak <nayak@keeunit.de>
2023-02-23 12:52:04 +0100

    pinia practice

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+
+
+<template>
+  <header>
+    <img src="./assets/logo.svg" />
+    
+    <!--new task form-->
+    
+    <TaskForm />
+    <!-- filter -->
+    
+    <nav>
+      <button class="s-Button" @click="filter = 'all' ">All</button>
+      <button class="s-Button" @click="filter = 'favs' ">Favs</button>
+    </nav>
+
+    <!--task lsits-->
+
+    <div v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <p class="s-Tasks">All tasks</p>
+      <div v-for="task in taskStore.tasks">
+      <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <p class="s-Tasks">Favs tasks</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import { useTaskStore } from './stores/TaskStore'
+import  TaskDetails  from './components/TaskDetails.vue'
+import  TaskForm from './components/TaskForm.vue'
+const taskStore = useTaskStore()
+
+const filter = ref('all');
+
+</script>
+
+<style scoped>
+.s-Button{
+  width:60px;
+  height: 30px;
+  margin:10px;
+  background-color:rebeccapurple;
+  border:none;
+  color:white;
+  font-size: 14px;
+  font-weight: bold;
+}
+.s-Tasks{
+  color:green;
+  font-size: 25px;
+  border-bottom: 5px solid black;
+  padding:10px;
+}
+</style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="s-Task__header" >
+        <h3 >{{ task.title }}</h3>
+        <div>
+            <i class="fa-solid fa-trash" @click="taskStore.deleteTask(task.id)"></i>
+            <i class="fa-solid fa-heart" @click="taskStore.toggleFav(task.id)"></i>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useTaskStore } from '../stores/TaskStore';
+    const props = defineProps({
+        task: Object
+    })
+    const taskStore = useTaskStore()
+
+
+</script>
+<style scoped>
+    .s-Task__header{
+        display:flex;
+        justify-content: space-between;
+        gap:10px;
+        height:40px;
+        width:100%;
+        margin-top:10px;
+        padding:5px;
+        border:2px solid #888888;
+        box-shadow: 1px 2px #888888;
+    }
+</style>
